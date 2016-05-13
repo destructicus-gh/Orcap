05763e6f0caf275f95f927ad38ca585912c7425a
a689638 <anders.ryan@heb.com>
2016-05-13 16:05:26 -0500

    also this

--- js/app.js
+++ js/app.js
@@ -1,15 +1,44 @@
-var routerApp = angular.module('orcap', ['ui.router', 'ngAnimate']);
+var routerApp = angular.module('orcap', ['ui.router', 'ngAnimate', 'permission', 'permission.ui']);
+
+routerApp.run(function (PermissionStore) {
+    PermissionStore
+        .definePermission('permissionA', function () {
+            return false;
+        });
+});
 
 routerApp.config(function ($stateProvider, $urlRouterProvider) {
     console.log("this ran");
-    $urlRouterProvider.otherwise('/home');
+
+    $urlRouterProvider.otherwise(function ($injector) {
+        var $state = $injector.get("$state");
+        $state.go('login', {message: "you can't go there"});
+    });
     $stateProvider
         .state('home', {
             url: '/home',
-            templateUrl: 'views/home.html'
+            templateUrl: 'views/home.html',
+            data: {
+                permissions: {
+                    only: 'permissionA',
+                    redirectTo: {
+                        default: {
+                            state: 'login',
+                            params: {
+                                message: 'Must Log in, Thanks.'
+                            }
+                        }
+                    }
+                }
+            }
         })
         .state('login', {
             url: '/login',
+            controller: 'LoginController',
+            controllerAs: "ctrl",
+            params: {
+                message: ""
+            },
             templateUrl: 'views/login.html'
         });
 
